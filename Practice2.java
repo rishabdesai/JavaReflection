f5ab5fa5f3aee3183cd7827b92d05473c8219d41
Rishabh Desai <desai.rishab@gmail.com>
2019-06-25 01:40:23 +0530

    Create Practice2.java
    getDeclaredFields. getDeclaredMethods, getDeclaredConstructors

---
+++ Practice2.java
@@ -0,0 +1,48 @@
+package demo7;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+class simple {
+	int a;
+	float b;
+	double c;
+
+	public simple() {
+		this.a = 0;
+		this.b = 0.0f;
+		this.c = 0.0;
+	}
+
+	void hello() {
+		System.out.println("hi");
+	}
+}
+
+public class Practice2 {
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class<?> c = Class.forName("demo7.simple");
+		System.out.println(c.getName());
+
+		Field f[] = c.getDeclaredFields();
+		for (Field f1 : f) {
+			System.out.println(f1);
+		}
+
+		Method[] m = c.getDeclaredMethods();
+		for (Method m1 : m) {
+			System.out.println(m1);
+		}
+
+		Constructor<?> con[] = c.getDeclaredConstructors();
+		for (Constructor<?> con1 : con) {
+			System.out.println(con1);
+		}
+		{
+
+		}
+
+	}
+
+}
